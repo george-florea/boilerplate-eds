64212175d965e32689812fabf7952bbbf298b947
George Florea <george.florea@netcentric.biz>
2025-06-23 15:09:20 +0300

    small changes

--- blocks/rating-stars/rating-stars.js
+++ blocks/rating-stars/rating-stars.js
@@ -1,9 +1,3 @@
-// window.addEventListener('DOMContentLoaded', (event) => {
-    let block = document.querySelector(".rating-stars div");
-    console.log("here");
-    let rating = block.querySelector("div:nth-child(3)");
-    console.log(rating)
-    debugger
-    rating.innerHTML = "*".repeat(parseInt(rating.innerText));
-    
-// });
+let block = document.querySelector(".rating-stars div");
+let rating = block.querySelector("div:nth-child(3)");
+rating.innerHTML = "*".repeat(parseInt(rating.innerText));
